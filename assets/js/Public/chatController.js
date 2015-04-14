f9f648b5861861ecff9696c08912d9c240ba81e3
matheusmig <matheus.mignoni@gmail.com>
2015-04-14 07:16:53 -0300

    Corrigido o carregamento do chatController e do chatModule

--- assets/js/Public/chatController.js
+++ assets/js/Public/chatController.js
@@ -1,3 +1,36 @@
 var socketApp = angular.module('socketApp').controller('ChatController',['$http','$log','$scope',function($http,$log,$scope){
-	// $http , $scope , $log injections
+	
+
+			$scope.getAllchat = function(){
+
+				io.socket.get('/chat/addConversation');
+
+				$http.get($scope.baseUrl+'/chat')
+					 .success(function(success_data){
+
+					 		$scope.chatList = success_data;
+					 		$log.info(success_data);
+					 });
+			};
+
+
+			io.socket.on('chat',function(obj){
+				//Check whether the verb is created or not
+				if(obj.verb === 'created'){
+					$log.info(obj)
+					$scope.chatList.push(obj.data);
+					// Add the data to current chatList
+					// Call $scope.$digest to make the changes in UI
+					$scope.$digest();
+				}
+			});
+
+			
+			$scope.sendMsg = function(){
+				$log.info($scope.chatMessage);
+				io.socket.post('/chat/addConversation/',{user:$scope.chatUser,message: $scope.chatMessage});
+				$scope.chatMessage = "";
+			};
+
+
 }]);
--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -29,6 +29,7 @@ var jsFilesToInject = [
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
 
+  'js/public/signup/chatModule.js'
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
   'js/**/*.js'
